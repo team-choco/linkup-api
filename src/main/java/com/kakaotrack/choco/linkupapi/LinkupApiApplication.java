1186b4fa8dee083c04881f49f1c815dd2f2cb52c
1jinju <gkswlswn2044@gmail.com>
2023-01-16 18:23:12 +0900

    first commit

---
+++ src/main/java/com/kakaotrack/choco/linkupapi/LinkupApiApplication.java
@@ -0,0 +1,13 @@
+package com.kakaotrack.choco.linkupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkupApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakaotrack/choco/linkupapi/LinkupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaotrack.choco.linkupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
